0915f836b144d48005d58f6f9462c44740bd285c
Tom Dilatush <tom@dilatush.com>
2017-04-02 15:25:26 -0600

    Started PragmaticDecimal.

--- src/com/dilatush/pragmatic/decimal/PragmaticDecimal.java
+++ src/com/dilatush/pragmatic/decimal/PragmaticDecimal.java
@@ -4,4 +4,12 @@ package com.dilatush.pragmatic.decimal;
  * @author Tom Dilatush  tom@dilatush.com
  */
 public class PragmaticDecimal {
+
+    // fields always present in every instance
+    private final byte[] digits;  // BCD mantissa digits
+
+
+    public PragmaticDecimal( final String num ) {
+        digits = new byte[0];
+    }
 }
